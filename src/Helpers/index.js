e9d3f20f3d17b8dfec82cbdff3103a512eb7a98e
Colin Lees <colin.lees@stockport.gov.uk>
2020-02-10 15:30:01 +0000

    fix(helpers) remove uneeded helper methods

--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -39,22 +39,7 @@ const fetchWithTimeout = (url, options, timeout = 10000) => {
     ])
 }
 
-/**
- * @param {*} circleMarker required
- * @param {*} content required
- */
-const createPopup = (circleMarker, content) => {
-    /**
-     * circleMarker may contain a _latlng property, otherwise we create our own latlng object
-     * circleMarker.feature.geometry.coordinates lat lng are reversed, so lat is in position [0][1] and lng is in position [0][0]
-     */
-    const latlng = circleMarker._latlng || {lat: circleMarker.feature.geometry.coordinates[0][1], lng: circleMarker.feature.geometry.coordinates[0][0]}
-
-    circleMarker.on('click', () => window.LeafletMap.fireEvent('openPopup', { content, latlng } ))
-}
-
 export {
     fetchWithTimeout,
-    getTargetUrl,
-    createPopup
+    getTargetUrl
 }
